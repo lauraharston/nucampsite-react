992f49d34163ce32930e9ea7ecfa413059c80721
lauraharston <lauraharston@gmail.com>
2020-12-29 09:04:24 -0600

    adding a SPA

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponents'
 import {CAMPSITES} from '../shared/campsites'
+import {COMMENTS} from '../shared/comments'
+import {PARTNERS} from '../shared/partners'
+import {PROMOTIONS} from '../shared/promotions'
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
 import Home from './HomeComponent'
+import Contact from './ContactComponent'
 import {Switch, Route, Redirect} from 'react-router-dom'
 import CampsiteInfo from './CampsiteInfoComponent'
 
@@ -12,6 +16,9 @@ class Main extends Component {
     super(props);
     this.state = {
       campsites: CAMPSITES,
+      comments: COMMENTS,
+      partners: PARTNERS,
+      promotions: PROMOTIONS
     };
   }
 
@@ -20,7 +27,11 @@ class Main extends Component {
   render() {
     const HomePage = () => {
       return (
-        <Home />
+        <Home 
+          campsite = {this.state.campsites.filter(campsite => campsite.featured)[0]}
+          promotion = {this.state.promotions.filter(promotion => promotion.featured)[0]}
+          partner = {this.state.partners.filter(partner => partner.featured)[0]}
+        />
       );
     }
 
@@ -30,6 +41,7 @@ class Main extends Component {
             <Switch>
                 <Route path='/home' component={HomePage}/>
                 <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                <Route exact path='/contactus' component={Contact} />
                 <Redirect to='/home' />
             </Switch>
             <Footer/>
